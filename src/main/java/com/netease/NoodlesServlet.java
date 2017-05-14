dab59ae76fd474293ba6a3036fcb071d7f356286
DerekZB <derek.zhangbin@gmail.com>
2017-05-14 14:17:05 +0800

    modify NoodlesServlet.java

--- src/main/java/com/netease/NoodlesServlet.java
+++ src/main/java/com/netease/NoodlesServlet.java
@@ -2,6 +2,7 @@ package com.netease;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import javax.servlet.ServletException;
@@ -9,8 +10,21 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class NoodlesServlet extends HttpServlet {
+import org.apache.log4j.Logger;
 
+public class NoodlesServlet extends HttpServlet {
+	private static Logger logger = Logger.getLogger(NoodlesServlet.class);  
+	
+	@Override
+	protected void service(HttpServletRequest arg0, HttpServletResponse arg1) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		Date currentTime = new Date();
+		SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		String dateString = formatter.format(currentTime);
+		logger.info(dateString);
+		super.service(arg0, arg1);
+	}
+	
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
         PrintWriter writer = response.getWriter();
@@ -23,4 +37,6 @@ public class NoodlesServlet extends HttpServlet {
         writer.println("<h1> Noodles with " + vegetable + "</h1>"); 
         writer.println("</body></html>");
     }
+
+
 }
